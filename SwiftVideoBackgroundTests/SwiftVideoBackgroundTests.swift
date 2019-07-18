181e78171ccc97c3f24cb77d3d629939c2dd904a
peterleng150 <45184849+peterleng150@users.noreply.github.com>
2019-07-18 12:25:25 -0400

    Update SwiftVideoBackgroundTests.swift

--- SwiftVideoBackgroundTests/SwiftVideoBackgroundTests.swift
+++ SwiftVideoBackgroundTests/SwiftVideoBackgroundTests.swift
@@ -1,10 +1,3 @@
-//
-//  SwiftVideoBackgroundTests.swift
-//  SwiftVideoBackgroundTests
-//
-//  Created by Wilson Ding on 9/28/16.
-//  Copyright © 2016 Wilson Ding. All rights reserved.
-//
 
 import AVKit
 import SwiftVideoBackground
